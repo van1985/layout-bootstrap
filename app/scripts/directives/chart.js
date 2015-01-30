18daeb328167f25548e5fc86f0b45a1cf8e2d28b
agustin veloso <agustin.veloso@globant.com>
2015-01-30 14:24:15 -0300

    statistics service implemented with directives

--- app/scripts/directives/chart.js
+++ app/scripts/directives/chart.js
@@ -4,42 +4,65 @@ angular.module('storeManagerApp')
 	.directive('chart', function() {
 		return {
             restrict: 'A',
+            scope: {
+            	stats: '='
+            },
             controller: function ($scope, $attrs) {
 
-				$scope.chart = new CanvasJS.Chart($attrs.id, {
-					theme: 'theme2',
-					height: 250,
-					width: 500,
-					backgroundColor: null,
-		            animationEnabled: true,
-		            axisY: {
-		                tickThickness: 0,
-		                lineThickness: 0,
-		                valueFormatString: " ",
-		                gridThickness: 0         
-		            },
-		            axisX: {
-		                tickThickness: 0,
-		                lineThickness: 0,
-		                labelFontSize: 20,
-		                labelFontColor: "#999999",
-		                labelFontFamily: "helvetica",
-		                gridThickness: 0 
-		            },
-		            data: [
-			            {
-			                type: "bar",
-			                dataPoints: [
-			                    { y: 15, label: "Missed", color: "#339966", indexLabelFontFamily:"helvetica",indexLabelFontColor:"white", indexLabel:"15           ", indexLabelPlacement: "inside" },
-			                    { y: 6, label: "Transferred", color: "#99CC66", indexLabelFontFamily:"helvetica",indexLabelFontColor:"white", indexLabel:"6  ", indexLabelPlacement: "inside" },
-			                    { y: 32, label: "Answered", color: "#3399CC", indexLabelFontFamily:"helvetica",indexLabelFontColor:"white", indexLabel:"32                            ", indexLabelPlacement: "inside" },
-			                    { y: 78, label: "Incoming", color: "#99CCCC", indexLabelFontFamily:"helvetica",indexLabelFontColor:"white", indexLabel:"78                                                                              ", indexLabelPlacement: "inside" }
-			                ]
-			            }
-		            ]
-				});
+            	$scope.$watch('stats', function() {
+            		if ($scope.stats) {
+            			$scope.chartData = $scope.stats;
+            			initializeChart();
+            		}
+            	});
 
-				$scope.chart.render(); //render the chart for the first time
+            	var getIndexLabel = function (label) {
+            		var indexLabel = label;
+
+            		for (var i = 6; i < label; i++) {
+            			indexLabel += " ";
+            		}
+
+            		return indexLabel;
+            	};
+
+            	var initializeChart = function() {
+					$scope.chart = new CanvasJS.Chart($attrs.id, {
+						theme: 'theme2',
+						height: 250,
+						width: 500,
+						backgroundColor: null,
+			            animationEnabled: true,
+			            axisY: {
+			                tickThickness: 0,
+			                lineThickness: 0,
+			                valueFormatString: " ",
+			                gridThickness: 0         
+			            },
+			            axisX: {
+			                tickThickness: 0,
+			                lineThickness: 0,
+			                labelFontSize: 20,
+			                labelFontColor: "#999999",
+			                labelFontFamily: "helvetica",
+			                gridThickness: 0 
+			            },
+			            data: [
+				            {
+				                type: "bar",
+				                dataPoints: [
+				                    { y: $scope.chartData.missed, label: "Missed", color: "#339966", indexLabelFontFamily:"helvetica",indexLabelFontColor:"white", indexLabel: getIndexLabel($scope.chartData.missed), indexLabelPlacement: "inside" },
+				                    { y: $scope.chartData.transferred, label: "Transferred", color: "#99CC66", indexLabelFontFamily:"helvetica",indexLabelFontColor:"white", indexLabel:getIndexLabel($scope.chartData.transferred), indexLabelPlacement: "inside" },
+				                    { y: $scope.chartData.answered, label: "Answered", color: "#3399CC", indexLabelFontFamily:"helvetica",indexLabelFontColor:"white", indexLabel:getIndexLabel($scope.chartData.answered), indexLabelPlacement: "inside" },
+				                    { y: $scope.chartData.incoming, label: "Incoming", color: "#99CCCC", indexLabelFontFamily:"helvetica",indexLabelFontColor:"white", indexLabel:getIndexLabel($scope.chartData.incoming), indexLabelPlacement: "inside" }
+				                ]
+				            }
+			            ]
+					});
+
+
+					$scope.chart.render(); //render the chart for the first time
+				}
 			}
 		};
 	});
--- app/scripts/directives/donut-chart.js
+++ app/scripts/directives/donut-chart.js
@@ -4,45 +4,89 @@ angular.module('storeManagerApp')
 	.directive('donutChart', function() {
 		return {
             restrict: 'A',
+            scope: {
+            	stats: '='
+            },
             controller: function ($scope, $attrs) {
 
-				$scope.chart = new CanvasJS.Chart($attrs.id, {
-					theme: 'theme2',
-					height: 250,
-					backgroundColor: null,
-		            animationEnabled: true,
-		            axisY: {
-		                tickThickness: 0,
-		                lineThickness: 5,
-		                valueFormatString: " ",
-		                gridThickness: 0                 
-		            },
-		            axisX: {
-		                tickThickness: 0,
-		                lineThickness: 0,
-		                labelFontSize: 20,
-		                labelFontColor: "Peru",
-		                interval: 1
-		            },
-		            data: [
-						{
-							type: "doughnut",
-							indexLabelFontSize: 16,
-					        indexLabelFontFamily: "helvetica",
-					        indexLabelFontColor: "#999999",
-					        indexLabelFontWeight: "normal",
-					        indexLabelFontStyle: "normal",
-							dataPoints: [
-								{  y: 32, indexLabel: "Books & Magazines",color: "#66CCCC", indexLabelLineColor: "66CCCC", indexLabelMaxWidth: 120, indexLabelWrap: true },
-								{  y: 36, indexLabel: "Gifts", color: "#339966", indexLabelLineColor: "339966", indexLabelMaxWidth: 120, indexLabelWrap: true },		
-								{  y: 17, indexLabel: "Clothing, Shoes & Jewelry", color: "#666666", indexLabelLineColor: "666666", indexLabelMaxWidth: 120, indexLabelWrap: true },						
-								{  y: 2, indexLabel: "All Other Departament", color: "#99CC66", indexLabelLineColor: "99CC66", indexLabelMaxWidth: 120, indexLabelWrap: true }
-							]
-						}
-					]
-				});
+            	$scope.$watch('stats', function() {
+            		if ($scope.stats) {
+            			$scope.chartData = $scope.stats;
+            			initializeChart();
+            		}
+            	});
 
-				$scope.chart.render(); //render the chart for the first time
+            	var initializeChart = function() {
+            		var dataPoints = [];
+
+            		for (var i = 0; i < $scope.chartData.length; i++) {
+            			var dataPoint = {
+            				y: $scope.chartData[i].callsAmount,
+            				indexLabel: $scope.chartData[i].departmentId,
+            				indexLabelMaxWidth: 120,
+            				indexLabelWrap: true
+            			};
+
+            			var color;
+            			switch (i%4) {
+            				case 0:
+            					color = "66CCCC";
+            					break;
+            				case 1:
+            					color = "339966";
+            					break;
+            				case 2:
+            					color = "666666";
+            					break;
+            				default:
+            					color = "99CC66";
+            			}
+
+            			dataPoint.color = "#" + color;
+            			dataPoint.indexLabelLineColor = color;
+
+            			dataPoints.push(dataPoint);
+            		}
+
+					$scope.chart = new CanvasJS.Chart($attrs.id, {
+						theme: 'theme2',
+						height: 250,
+						backgroundColor: null,
+			            animationEnabled: true,
+			            axisY: {
+			                tickThickness: 0,
+			                lineThickness: 5,
+			                valueFormatString: " ",
+			                gridThickness: 0                 
+			            },
+			            axisX: {
+			                tickThickness: 0,
+			                lineThickness: 0,
+			                labelFontSize: 20,
+			                labelFontColor: "Peru",
+			                interval: 1
+			            },
+			            data: [
+							{
+								type: "doughnut",
+								indexLabelFontSize: 16,
+						        indexLabelFontFamily: "helvetica",
+						        indexLabelFontColor: "#999999",
+						        indexLabelFontWeight: "normal",
+						        indexLabelFontStyle: "normal",
+								/*dataPoints: [
+									{  y: 32, indexLabel: "Books & Magazines",color: "#66CCCC", indexLabelLineColor: "66CCCC", indexLabelMaxWidth: 120, indexLabelWrap: true },
+									{  y: 36, indexLabel: "Gifts", color: "#339966", indexLabelLineColor: "339966", indexLabelMaxWidth: 120, indexLabelWrap: true },		
+									{  y: 17, indexLabel: "Clothing, Shoes & Jewelry", color: "#666666", indexLabelLineColor: "666666", indexLabelMaxWidth: 120, indexLabelWrap: true },						
+									{  y: 2, indexLabel: "All Other Departament", color: "#99CC66", indexLabelLineColor: "99CC66", indexLabelMaxWidth: 120, indexLabelWrap: true }
+								]*/
+								dataPoints: dataPoints
+							}
+						]
+					});
+
+					$scope.chart.render(); //render the chart for the first time
+				}
 			}
 		};
 	});
